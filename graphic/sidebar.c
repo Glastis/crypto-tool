459ead16aad0cf008acaefb63ac34259bacc9920
Glastis <glastis@glastis.com>
2021-10-10 12:18:09 +0200

    Fixed sidebar

--- graphic/sidebar.c
+++ graphic/sidebar.c
@@ -4,38 +4,41 @@
 
 #include "sidebar.h"
 
-static void             create_sidebar_stack_add_label(GtkWidget *stack, const char *name, const char *value)
+static void             create_sidebar_stack_add_label(GtkStack *stack, const char *name, const char *value)
 {
     GtkWidget           *label;
 
-    label = gtk_label_new_with_mnemonic(value);
-    gtk_stack_add_titled(GTK_STACK(stack), label, name, value);
+    label = gtk_label_new(value);
+    gtk_stack_add_titled(stack, label, name, value);
 }
 
-static GtkWidget        *create_sidebar_stack(GtkWidget *grid)
+static GtkStack         *create_sidebar_stack(GtkWidget *grid)
 {
-    GtkWidget           *stack;
+    GtkStack            *stack;
 
-    stack = gtk_stack_new();
-    gtk_stack_set_homogeneous(GTK_STACK(stack), gtk_true());
-    gtk_grid_attach(GTK_GRID(grid), stack, 1, 0, 1, 1);
+    puts(__func__);
+    stack = GTK_STACK(gtk_stack_new());
+    gtk_stack_set_homogeneous(stack, gtk_true());
+    puts(__func__);
+    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(stack), 1, 0, 1, 1);
     create_sidebar_stack_add_label(stack, "1", "coucou");
     create_sidebar_stack_add_label(stack, "2", "coucoumdr");
     create_sidebar_stack_add_label(stack, "3", "jambon");
     create_sidebar_stack_add_label(stack, "4", "cornichon");
-    return grid;
+    return stack;
 }
 
 GtkWidget               *create_sidebar()
 {
     GtkWidget           *sidebar;
-    GtkWidget           *stack;
+    GtkStack            *stack;
     GtkWidget           *grid;
 
     grid = gtk_grid_new();
     sidebar = gtk_stack_sidebar_new();
+    gtk_widget_set_vexpand(sidebar, TRUE);
     stack = create_sidebar_stack(grid);
-    gtk_stack_sidebar_set_stack(GTK_STACK_SIDEBAR(sidebar), GTK_STACK(stack));
+    gtk_stack_sidebar_set_stack(GTK_STACK_SIDEBAR(sidebar), stack);
     gtk_grid_attach(GTK_GRID(grid), sidebar, 0, 0, 1, 1);
     return grid;
 }
